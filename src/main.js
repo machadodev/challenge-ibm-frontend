a2babad358b6442c7a0e59d720e9b6f476e8c7fe
Leonardo Machado <leonardoms@id.uff.br>
2021-10-14 12:41:35 -0300

    add Carbon Design close #1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import 'carbon-components/css/carbon-components.css';
+import CarbonComponentsVue from '@carbon/vue/src/index';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(CarbonComponentsVue)
+  .mount('#app');
