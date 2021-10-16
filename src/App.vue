241544206fe781b4ae402cd1d8034a82af3a761a
Leonardo Machado <leonardoms@id.uff.br>
2021-10-16 17:38:04 -0300

    remove floatint btn and implement vlibras

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,32 @@
     <CvContent id="#main-content" class="app-content">
       <router-view />
     </CvContent>
+
+    <!-- Adiciona o componente vlibras ao projeto -->
+    <VLibras></VLibras>
+
+    <!-- Adiciona botão na parte inferior direita de
+         acessibilidade com opções de contraste, tamanho de fonte, etc.
+    -->
+    <AccessibilityToolbar
+      invertColorsText="Inverter cores"
+      highlightLinksText="Focar Links"
+      accessibileFontSizeText="Mudar tamanho da fonte"
+    ></AccessibilityToolbar>
   </div>
 </template>
 
 <script>
 import { CvContent } from '@carbon/vue';
 import Header from './components/Header';
+import VLibras from './components/vlibras';
 
 export default {
   name: 'App',
   components: {
+    Header,
     CvContent,
-    Header
+    VLibras
   }
 };
 </script>
--- src/components/accessibility/README.txt
+++
@@ -1,3 +0,0 @@
-Esse componente é um FORK do seguinte repositório:
-
-https://github.com/hendrixfan/vue-accessibility-toolbar
